5741aadacaa52e314886611ede35e76b2704d7f0
detechcompany <hjoh@detech.co.kr>
2023-03-09 17:09:12 +0900

    테스트 코드 확인

--- __test__/components/Jest.test.tsx
+++ __test__/components/Jest.test.tsx
@@ -10,13 +10,8 @@ import DndDashboardLayout from '../../app/components/layout/DndDashboardLayout';
 import axios from 'axios';
 import { useAsync } from "react-async";
 import * as HttpUtil from '../../app/utils/api/HttpUtil';
-import MockAdapter from 'axios-mock-adapter';
 import { APIBASE_DEV } from '../../app/utils/api/HttpConf';
 
-// axios 인스턴스 생성
-const instance = axios.create({
-    baseURL: APIBASE_DEV,
-});
 
 // 다국어 적용
 jest.mock('react-i18next', () => ({
@@ -24,16 +19,15 @@ jest.mock('react-i18next', () => ({
 }));
 // API 적용
 jest.mock('date-fns');
-jest.mock("axios");
-const mock = new MockAdapter(instance);
+jest.mock(`axios`);
+jest.spyOn(axios,'create')
 describe('로그인 테스트', () => {
-
-
-    afterEach(() => {
-        mock.reset();
-    });
-    // const 
     it('로그인 성공 테스트', async () => {
+        const data = {
+            userId: "test@test.com",
+            userPw: "1234qwer!@"
+        }
+        const login = '/api/v2/auth/login'
         const { rerender } = render(
             <MemoryRouter>
                 <RecoilRoot>
@@ -43,14 +37,6 @@ describe('로그인 테스트', () => {
 
         );
 
-        const data = {
-            userId: "test@test.com",
-            userPw: "1234qwer!@"
-        }
-        const loginResponse = {
-            status: "success",
-            token: "testtoken"
-        }
 
         const userId = screen.getByLabelText('아이디 입력') as HTMLInputElement;
         const userPw = screen.getByLabelText('비밀번호 입력') as HTMLInputElement;
@@ -62,23 +48,18 @@ describe('로그인 테스트', () => {
 
 
         await act(async () => {
-            const { result } = renderHook(() => useAsync({
-                deferFn: HttpUtil.Http,
-                httpMethod: "POST",
-                appPath: "/api/v2/auth/login",
-                appQuery: {
-                    userId: userId.value,
-                    password: userPw.value,
-                },
+            const logs = axios.post(`${APIBASE_DEV+login}`,{
+                data:{
+                    userId:userId.value,
+                    userPw:userPw.value
+                }
             })
-            );
-            console.log("gdata", JSON.stringify(result.error));
-            expect(result).toBeTruthy();
+            console.log("api",logs);
+            
             expect(userId.value).toEqual(data.userId)
             expect(userPw.value).toEqual(data.userPw)
             expect(button).toBeEnabled()
-
-
+            
             waitFor(() => {
                 const successMessage = window.location.pathname;
                 expect(successMessage).toBe("/");
@@ -92,58 +73,10 @@ describe('로그인 테스트', () => {
             )
         });
     });
-    // it('로그인 실패 테스트', async () => {
 
-    //     const userId = screen.getByLabelText('아이디 입력') as HTMLInputElement;
-    //     const userPw = screen.getByLabelText('비밀번호 입력') as HTMLInputElement;
-    //     const button = screen.getByRole('button', { name: '로그인' }) as HTMLButtonElement;
-
-    //     fireEvent.change(userId, { target: { value: 'test@test.com' } });
-    //     fireEvent.change(userPw, { target: { value: '1234qwer!@' } });
-    //     fireEvent.click(button);
-
-    //     expect(userId.value).toEqual("test@test.com")
-    //     expect(userPw.value).not.toEqual("11111111")
-    //     expect(button).toBeEnabled()
-
-    //     await waitFor(() => {
-    //         const successMessage = window.location.pathname;
-    //         expect(successMessage).toBe("/");
-    //         act(() => {
-    //             rerender(
-    //                  <MemoryRouter>
-    //                      <RecoilRoot>
-    //                          <DndDashboardLayout />
-    //                      </RecoilRoot>
-    //                  </MemoryRouter>
-    //              )
-    //          });
-    //     });
-    // });
 
 
 
 
 });
 
-
-/* 
-HttpUtil.PromiseHttp({
-      httpMethod: "POST",
-      appPath: "/api/v2/user",
-      appQuery: {
-        userId: userId,
-        password: password + confirmPassword, // password 와 confirmPassword 가 다르면 error 만든 비동기 api
-        userName: userName,
-        phoneNumber: phoneNumber,
-        companyName: companyName,
-        zoneName: zoneName,
-        department: department,
-        classificationCode: classificationCode, // 20221031 업종 추가
-        agreeTos: strAgreeTos,
-        agreePersonalInfo: strAgreePersonalInfo,
-        agreeData: strAgreeData,
-        // agreeMailReceipt: strAgreeMailReceipt,
-        language: apiLang
-      },
-*/
